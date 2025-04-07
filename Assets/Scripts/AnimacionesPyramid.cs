815d6e381106bacc64b77b34026f3019f7245814
Bautista-Diaz-DaVinci-1 <bautista.diaz@davinci.edu.ar>
2025-04-07 12:10:18 -0300

    Piramide en camara

--- Assets/Scripts/AnimacionesPyramid.cs
+++ Assets/Scripts/AnimacionesPyramid.cs
@@ -1,14 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class AnimacionesPyramid : MonoBehaviour
 {
     [SerializeField] ParticleSystem chispas;
     [SerializeField] GameObject top;
 
+    [SerializeField] Animator anim;
+
+    private void Start()
+    {
+        EventManager.Instance.Register(GameEventTypes.OnGameplay, RestartAnim);
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.Instance.Register(GameEventTypes.OnGameplay, RestartAnim);
+    }
+
     void Close()
     {
         Instantiate(chispas, top.transform);
+    }
+
+    void RestartAnim(object sender, EventArgs e)
+    {
+        Debug.Log("Reinicia anim piramide");
+        anim.Play("PyramidStartLevitation", -1, 0f);
     }
 }
